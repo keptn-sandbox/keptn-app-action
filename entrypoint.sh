98860666e527b95a9523a8599dc97b767084809e
Thomas Schuetz <thomas.schuetz@dynatrace.com>
2023-02-27 08:12:46 +0100

    updated action
    Signed-off-by: Thomas Schuetz <thomas.schuetz@dynatrace.com>

---
+++ entrypoint.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+set -e
+while getopts "i:o:v:b:" o; do
+   case "${o}" in
+      i)
+        export input=${OPTARG}
+        ;;
+      o)
+        export output=${OPTARG}
+        ;;
+      v)
+        export version=${OPTARG}
+        ;;
+      b)
+        export bump=${OPTARG}
+        ;;
+  esac
+done
+
+ARGS=""
+
+if [ -n "$input" ]; then
+  ARGS="$ARGS --input $input"
+fi
+
+if [ -n "$output" ]; then
+  ARGS="$ARGS --output $output"
+fi
+
+if [ -n "$version" ]; then
+  ARGS="$ARGS --version $version"
+fi
+
+if [ -n "$bump" ]; then
+  ARGS="$ARGS --bump $bump"
+fi
+
+echo "Running \'go run cmd/keptn-update-action/main.go ${ARGS}\'"
+go run cmd/keptn-update-action/main.go ${ARGS}
+
+
+
